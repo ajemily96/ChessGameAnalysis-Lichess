3f0ca42d5b03dc2de352bb3e8e2dab634209c7a9
Ava Wiegert <aemily6296@gmail.com>
2021-04-19 20:50:50 -0500

    changes to SQLite

--- static/py/CreateAndPopulateDataBase.py
+++ static/py/CreateAndPopulateDataBase.py
@@ -5,81 +5,102 @@ import pandas as pd
 import sqlalchemy
 # from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, func, inspect
-from sqlalchemy import Column, Integer, String, Float 
+from sqlalchemy import Table, Column, Integer, String, Float 
+from sqlalchemy import MetaData
+from sqlalchemy import insert
 
 import datetime as dt
 
 import matplotlib.pyplot as plt
 
 # Database Setup
-engine = create_engine("sqlite:///../../data/chessdatalichness.sqlite")
+engine = create_engine("sqlite:///data/chessdatalichness.sqlite")
 
 # reflect an existing database into a new model
 Base = declarative_base()
 
-# inspect DB to see what tables we have
+#Create a metadata instance
+metadata=MetaData(engine)
+
+# open session
+session = Session(engine)
+# Session=sessionmaker(bind=engine)
+# session=Session()
+
+# Create the inspector and connect it to the engine
 inspector = inspect(engine)
 
 # check out what tables exist
 inspector.get_table_names()
 
-# Creates Classes which will serve as the anchor points for our Tables
-class CHESS_DATA(Base):
-    __tablename__ = 'CHESS_DATA'
+# Define structure of table
+class CHESS_DATA():
+    def __init__(self, id, format, victory_status, book_moves, opening_name, winner, turns, white_id, white_rating, black_id, black_rating):
+        self.id = id 
+        self.format = format
+        self.victory_status = victory_status
+        self.book_moves = book_moves
+        self.opening_name = opening_name
+        self.winner = winner
+        self.turns = turns
+        self.white_id = white_id
+        self.white_rating = white_rating
+        self.black_id = black_id
+        self.black_rating = black_rating
 
-
-    id = Column(Integer, primary_key=True)
-    format = Column(String(10))
-    victory_status = Column(String(50))
-    book_moves = Column(Integer)
-    opening_name = Column(String(250))
-    winner = Column(String(5))
-    turns = Column(Integer)
-    white_id = Column(String(10))
-    white_rating = Column(Integer)
-    black_id = Column(String(10))
-    black_rating = Column(Integer)
-
+# Create (if not already in existence) the tables associated with our classes.
+Base.metadata.create_all(engine)
+   
+# Declaring a table 
+Table('CHESS_DATA',metadata, 
+Column('id', String(250), primary_key=True),
+Column('format', String(250)),
+Column('victory_status', String(250)),
+Column('book_moves', Integer),
+Column('opening_name', String(500)),
+Column('winner', String(250)),
+Column('turns', Integer),
+Column('white_id', String(250)),
+Column('white_rating', Integer),
+Column('black_id', String(250)),
+Column('black_rating', Integer)
+)
 
 # Create (if not already in existence) the tables associated with our classes.
 Base.metadata.create_all(engine)
 
-# Create function for loading data
-def Load_Data(file_name):
-    data = np.genfromtxt(file_name, delimiter=',', skip_header=1, converters={0: lambda s: str(s)})
-    return data.tolist()
+# inspect DB to see what tables we have
+inspector = inspect(engine)
 
-# open session
-session = Session(engine)
+# # Create function for loading data
+# def Load_Data(file_name):
+#     data = np.genfromtxt(file_name, delimiter=',', skip_header=1, converters={0: lambda s: str(s)})
+#     return data.tolist()
 
 # load the data from CSV
-file_name = "../../data/CleanedData.csv" 
-data = Load_Data(file_name) 
+file_name = "data/chess.csv" 
+# data = Load_Data(file_name)
+data = pd.read_csv(file_name) 
+
+# for i in data:
+#     conn = engine.connect()
+#     ins = CHESS_DATA.insert().values( {'id':i[0], 'format' : i[1], 'victory_status' : i[2], 'book_moves' : i[3], 'opening_name' : i[4], 'winner' : i[5], 'turns' : i[6], 'white_id' : i[7], 'white_rating' : i[8], 'black_id' : i[9], 'black_rating' : i[10]})
+#     conn.execute(ins)
 
 for i in data:
-    record = CHESS_DATA(**{
-        'id' : i[0],
-        'format' : i[1],
-        'victory_status' : i[2],
-        'book_moves' : i[3],
-        'opening_name' : i[4],
-        'winner' : i[5],
-        'turns' : i[6],
-        'white_id' : i[7],
-        'white_rating' : i[8],
-        'black_id' : i[9],
-        'black_rating' : i[10]
-    })
-    session.add(record) #Add all the records
+    record = CHESS_DATA(id = i[0], format = i[1], victory_status = i[2], book_moves = i[3], opening_name = i[4], winner = i[5], turns = i[6], white_id = i[7], white_rating = i[8], black_id = i[9], black_rating = i[10])
+    #Add all the records
+    session.add(record)
 
-session.commit() #Attempt to commit all the records
+#Attempt to commit all the records
+session.commit() 
 
-# query data from table
-game_list = session.query(CHESS_DATA)
-for game in game_list:
-    print(game.game_id)
+# # query data from table
+# game_list = session.query(CHESS_DATA)
+# for game in game_list:
+#     print(game.game_id)
 
 # close session
 session.close()
